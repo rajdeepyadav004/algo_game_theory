3ac7a3e2abf163749e8765e8c1f6efc3d5abd9b8
Rajdeep Yadav <rajdeepyadav004@gmail.com>
2020-03-24 03:12:01 +0530

    Implemented functions using arg_min/arg_max
    Implemented best_response, max_min, min_max using arg_min and arg_max
    Also, max_min returns indices now

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 
 
+def get_row(matrix, num_row):
+	return matrix[num_row]
+
 def get_column(matrix, num_col):
 	return [row[num_col] for row in matrix] 
 
@@ -52,44 +55,20 @@ class game:
 		'''best response of player 1'''
 		'''player num can be 1 or 2'''
 		'''move represents the move of the other player'''
+		return arg_max(get_column(self.utility_1, move)) if player_num == 1 else arg_max(get_row(self.utility_2, move))
 
-
-		best_response_move = 0
-
-		if(player_num == 1):
-
-			current_max = self.utility_1[0][move]
-			for i in range(self.num_columns):
-				if (self.utility_1[i][move] > current_max):
-					current_max = self.utility_1[i][move]
-					best_response_move = i
-
-			current_max = self.utility_2[move][0]
-			for i in range(self.num_rows):
-				if (self.utility_2[move][i] > current_max):
-					current_max = self.utility_2[move][i]
-					best_response_move = i 
-
-		'''returns the number of best response'''
-		return best_response_move
 
 	def max_min(self, player_num):
 		if player_num==1:
-			return max([min(x) for x in self.utility_1])
+			return arg_max([min(get_row(self.utility_1, i)) for i in range(self.num_rows)])
 		else:	
-			min_of_columns = [0]*self.num_columns
-			for i in range(self.num_columns):
-				min_of_columns[i] = min([x[i] for x in self.utility_2])
-			return max(min_of_columns)
+			return arg_max([min(get_column(self.utility_2, i)) for i in range(self.num_columns)])
 
 	def min_max(self, player_num):
-		if player_num == 1:
-			max_of_rows = [0]*self.num_rows
-			for i in range(self.num_rows):
-				max_of_rows[i] = max([x[i] for x in self.utility_1])
-			return min(max_of_rows)
-		else:
-			return min([max(x) for x in self.utility_2])
+		if player_num==1:
+			return arg_min([max(get_column(self.utility_1, i)) for i in range(self.num_rows)])
+		else:	
+			return arg_min([max(get_row(self.utility_2, i)) for i in range(self.num_columns)])
 		
 
 	def print_game(self):
@@ -114,3 +93,7 @@ if __name__ == '__main__':
 
 	game1.print_game()
 	
+	print(game1.min_max(1))
+	print(game1.min_max(2))
+	print(game1.max_min(1))
+	print(game1.max_min(2))
